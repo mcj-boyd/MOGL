0ddbe1ad7df9b5a982a55e7aaa5cc91e95a0009a
Matthew Boyd <boydm@algonquincollege.com>
2015-01-07 14:31:18 -0500

    Initial Push

---
+++ README.txt
@@ -0,0 +1,8 @@
+Hey Martine,
+
+The IDs and Class names are a bit of a mess, I was planning on fixing them up later. Please change them if you find that they are not proper, I am still a little confused on when to use an ID and Class. This may make the JQuery unusable, but you can remove it from the code for now and I can plug it back in later. Email me with any questions and I will try to respond as fast as I can.
+
+Sorry again about not being able to make it to the meeting.
+
+Matthew
+
---
+++ scripts.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+    var elapsetime = 0;
+    var timerrunning = false;
+    var interval = null;
+    var progressatt = "";
+    
+    function timeinttostring(){
+        elapsetime++;
+        var hours   = Math.floor(elapsetime / 3600);
+        var minutes = Math.floor((elapsetime - (hours * 3600)) / 60);
+        var seconds = elapsetime - (hours * 3600) - (minutes * 60);
+
+        if (hours   < 10) {hours   = "0"+hours;}
+        if (minutes < 10) {minutes = "0"+minutes;}
+        if (seconds < 10) {seconds = "0"+seconds;}
+        var time    = hours+':'+minutes+':'+seconds;
+        progressatt = elapsetime + '%';
+        console.log(progressatt);
+        $('#progressbar-1').animate({"background-size":progressatt}, "slow");
+        $('#elapsed-time-1').text(time); 
+    }
+    
+    $('#contact-info-1').hide();
+    $('#project-description-1').hide();
+    $('#project-timer-1').hide();
+    
+    $('#company-name-1').click( function(){
+        if ($('#project-timer-1').css('display') == 'none') {
+            $('#project-timer-1').slideDown("slow");
+            $('#project-arrow-1').attr("src", "images/arrow-big-up.png");
+        }
+        if ($('#contact-info-1').css('display') == 'none') {
+            $('.collap-item').slideUp("slow");
+            $('#contact-info-1').slideDown("slow");
+        } else {
+            $('#contact-info-1').slideUp("slow");
+        }
+           
+        
+    });
+    
+    $('#project-name-1').click( function(){
+        if ($('#project-timer-1').css('display') == 'none') {
+            $('#project-timer-1').slideDown("slow");
+            $('#project-arrow-1').attr("src", "images/arrow-big-up.png");
+        }
+        if ($('#project-description-1').css('display') == 'none') {
+            $('.collap-item').slideUp("slow");
+            $('#project-description-1').slideDown("slow");
+        } else {
+            $('#project-description-1').slideUp("slow");
+        }
+//        $('.project-item').hide();
+//        $('#project-description').slideDown("slow");
+    });
+    
+    $('#play-pause-1').click( function () {
+        if(timerrunning == false){
+            $(this).attr("src", "images/pause.jpg");
+            interval = setInterval(timeinttostring, 1000);
+            timerrunning = true;
+        }else{
+            $(this).attr("src", "images/play.jpg");
+            clearInterval(interval);
+            timerrunning = false;
+        }
+    });
+    
+    $('#project-arrow-1').click( function () {
+        if ($('#project-timer-1').css('display') == 'none') {
+            $('#project-timer-1').slideDown("slow");
+            $(this).attr("src", "images/arrow-big-up.png");
+        }else{
+            $('.collap-item').slideUp("slow");
+            if(timerrunning == false){
+                $('#project-timer-1').slideUp("slow");
+                $(this).attr("src", "images/arrow-big-down.png");
+            }
+        }
+    });
+
+});
+    
